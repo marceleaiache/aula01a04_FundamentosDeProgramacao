74bd91e172f0ca81b35ec49fa5ced8a13b1e6fb0
marceleaiache <tuxmarcele@gmail.com>
2024-05-05 23:38:31 -0300

    initial commit

---
+++ 03-minibrowser/map.js
@@ -0,0 +1,29 @@
+// Explicação prática do map
+// Utilizar o shift + ctrl + p -> quokka new javascritp file
+
+// TDD - Test Driven Development
+
+
+var novoMapa = new Map();
+
+class HistoryMap {
+
+}
+
+// 11/11/2011 as 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL', {
+    title: 'Título do site',
+    url: 'url sozinha',
+});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+
+var resultado = novoMapa;
+console.log(resultado);
+
+
